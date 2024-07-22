f7f67f7aaa9e823e74b65d9d96fa3ac101519109
sylvieshimwauwase <slvshimwa@gmail.com>
2024-07-22 14:15:41 +0200

    adding missing diles

--- utils/db.js
+++ utils/db.js
@@ -1,41 +1,34 @@
-const { MongoClient } = require('mongodb');
-const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
+import { MongoClient } from 'mongodb';
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    const host = DB_HOST || 'localhost';
-    const port = DB_PORT || 27017;
-    const database = DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
-
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.client.connect()
-      .then(() => {
-        this.db = this.client.db(database);
-      })
-      .catch((err) => {
-        console.error(`MongoDB connection error: ${err}`);
-      });
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   isAlive() {
-    return this.client && this.client.isConnected();
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    if (!this.isAlive()) {
-      return 0;
-    }
-    const usersCollection = this.db.collection('users');
-    return usersCollection.countDocuments();
+    const users = this.db.collection('users');
+    const usersNum = await users.countDocuments();
+    return usersNum;
   }
 
   async nbFiles() {
-    if (!this.isAlive()) {
-      return 0;
-    }
-    const filesCollection = this.db.collection('files');
-    return filesCollection.countDocuments();
+    const files = this.db.collection('files');
+    const filesNum = await files.countDocuments();
+    return filesNum;
   }
 }
 
--- utils/redis.js
+++ utils/redis.js
@@ -1,53 +1,44 @@
-const redis = require('redis');
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
+// class to define methods for commonly used redis commands
 class RedisClient {
-      constructor() {
-            this.client = redis.createClient();
-            this.client.on('error', (error) => {
-                  console.log(`Redis client not connected to the server: ${error.message}`);
-            });
-      }
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
 
-      isAlive() {
-            return this.client.connected;
-      }
+  // check connection status and report
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
 
-      async get(key) {
-            return new Promise((resolve, reject) => {
-                  this.client.get(key, (err, value) => {
-                        if (err) {
-                              reject(err);
-                        } else {
-                              resolve(value);
-                        }
-                  });
-            });
-      }
+  // get value for given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
 
-      async set(key, value, duration) {
-            return new Promise((resolve, reject) => {
-                  this.client.set(key, value, 'EX', duration, (err) => {
-                        if (err) {
-                              reject(err);
-                        } else {
-                              resolve(true);
-                        }
-                  });
-            });
-      }
+  // set key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
 
-      async del(key) {
-            return new Promise((resolve, reject) => {
-                  this.client.del(key, (err) => {
-                        if (err) {
-                              reject(err);
-                        } else {
-                              resolve(true);
-                        }
-                  });
-            });
-      }
+  // del key vale pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
 }
 
 const redisClient = new RedisClient();
+
 module.exports = redisClient;
